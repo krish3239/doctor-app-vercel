48bb4f3e147cc31e105711dcf6aec049d1962432
krish3239 <krishbhardwaj3239@gmail.com>
2025-04-22 21:20:10 +0530

    the text area is corrected and columns also

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -61,13 +61,15 @@ export const columns: ColumnDef<Appointment>[] = [
 
       return (
         <div className="flex items-center gap-3">
-          <Image
-            src={doctor?.image!}
-            alt="doctor"
-            width={100}
-            height={100}
-            className="size-8"
-          />
+          {doctor?.image && (
+            <Image
+              src={doctor.image}
+              alt="doctor"
+              width={100}
+              height={100}
+              className="size-8"
+            />
+          )}
           <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
         </div>
       );
